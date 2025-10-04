5a6b3653bbf3cce736b633f521384c4c35a520a7
Alexis <alexis.reyes@weareinsolite.com>
2025-10-04 10:42:53 +0200

    Clean repo: remove virtual environment and add .gitignore

---
+++ ibm/ibmcloud.py
@@ -0,0 +1,9 @@
+
+
+from qiskit_ibm_runtime import QiskitRuntimeService
+
+QiskitRuntimeService.save_account(
+    channel="ibm_cloud",
+    token="0FbnEoyfwPSePDJTVb9mu_pULm2Mkr-xgjM27AR5cPaV",
+    instance="crn:v1:bluemix:public:quantum-computing:us-east:a/c5ca169d85684d1cb3927636fe4e0b7a:9af0c953-e4d1-4f60-9cdf-5cc9a190348d::"
+)
